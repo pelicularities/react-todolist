b5eb1b88c268e5bae93e85e87a404583ee8bf44b
Grace Teng <pelicularities@users.noreply.github.com>
2021-02-01 14:33:08 +0800

    add state and display method to Todolist

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
 
 class Todolist extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        { name: "Buy Milk", isDone: false },
+        { name: "Do push up", isDone: true },
+      ],
+    };
+  }
+
+  displayTodos() {
+    return this.state.todos.map((todo) => (
+      <div>
+        <span>{todo.name}</span>
+        <span>{todo.isDone ? " - completed" : " - not completed"}</span>
+      </div>
+    ));
+  }
+
   render() {
     return (
       <div>
         <div>Todolist</div>
+        <div>{this.displayTodos()}</div>
       </div>
     );
   }
