acd7879f5930f79de72db569ecea186785fc4d09
Grace Teng <pelicularities@users.noreply.github.com>
2021-02-01 14:26:25 +0800

    Add placeholder for todolist component

---
+++ src/components/Todolist.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Todolist extends Component {
+  render() {
+    return (
+      <div>
+        <div>Todolist</div>
+      </div>
+    );
+  }
+}
+
+export default Todolist;
